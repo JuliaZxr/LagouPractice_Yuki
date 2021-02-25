d9e58cebc5c3d45a29cab85d7990dd1c2a961a53
Julia <785533217@qq.com>
2021-02-25 15:20:40 +0800

    pytest-allure-feature
    笛卡尔集参数化相关代码

--- testing/test_allure_feature_story.py
+++ testing/test_allure_feature_story.py
@@ -10,11 +10,6 @@ class TestLogin():
         print("这是登录：测试用例，登录成功")
         pass
 
-    @allure.story("用户名缺失")
-    def test_login_success_a(self):
-        print("用户名缺失")
-        pass
-
     @allure.story("登录失败")
     def test_login_failure(self):
         with allure.step("点击用户名"):
@@ -26,4 +21,21 @@ class TestLogin():
             print("这是登录：测试用例，登录成功")
             assert "1" == 1
             print("登录失败")
+        pass
+
+    @allure.story("用户名缺失")
+    def test_login_success_a(self):
+        print("用户名缺失")
+        pass
+
+    @allure.story("登录失败")
+    def test_login_failure_a(self):
+        print("密码错误")
+        assert '2' == 1
+        pass
+
+    @allure.story("登录失败")
+    def test_login_failure_b(self):
+        print("账号不存在")
+        assert '2' == 1
         pass
--- testing/test_param.py
+++ testing/test_param.py
@@ -6,7 +6,7 @@ import pytest
 
 
 # 笛卡尔积，用来生成多个用例组合
-@pytest.mark.parametrize("a", [1, 2, 3, 4])
-@pytest.mark.parametrize("b", [4, 5, 6, 7, "a", "b"])
+@pytest.mark.parametrize("a", [1, 2, 3])
+@pytest.mark.parametrize("b", [4, 5, "x"])
 def test_case2(a, b):
     print(f"a= {a} b= {b}")
